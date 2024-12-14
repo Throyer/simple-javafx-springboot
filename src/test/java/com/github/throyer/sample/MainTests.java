c84cca28be415b6bf51c5612b85b3a3c3b83d83d
Renato Henrique <renato.henrique_98@hotmail.com>
2024-12-14 17:00:59 -0300

    change imports

--- src/test/java/com/github/throyer/sample/MainTests.java
+++ src/test/java/com/github/throyer/sample/MainTests.java
@@ -2,7 +2,6 @@ package com.github.throyer.sample;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
